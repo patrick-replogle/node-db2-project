7212f8094355f3705700232a94c0dd5902475153
patrick-replogle <patrickr1138@gmail.com>
2020-01-29 18:45:28 -0800

    added knexfile, basic route file, and a utils folder.

---
+++ cars/cars-router.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const db = require("../utils/db");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).json({ message: "Please try again later" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).json({ message: "Please try again later" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).json({ message: "Please try again later" });
+  }
+});
+
+module.exports = cars;
